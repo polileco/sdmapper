d47cd62dd814df5d88ffd1ed43bd9beeb776b670
polileco <leandro.polimeno@gmail.com>
2023-11-02 13:02:01 -0300

    First commit

---
+++ makerom.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if [[ -z $VERSION || -z $NEXTOR ]]; then
+ echo "Variables not set."
+ echo "Ex.: export VERSION=1.0.5 NEXTOR=2.1.1 && ./makerom.sh"
+ exit 1
+fi
+
+if [[ -f ./firmware/sdmapper_v${VERSION}.rom ]]; then
+ echo "./firmware/sdmapper_v${VERSION}.rom already exist. Delete it before make a new one."
+ exit 1
+fi
+ 
+./mknexrom ./nextor/Nextor-${NEXTOR}.base.dat ./firmware/sdmapper_v${VERSION}.rom \
+  /d:device.bin \
+  /m:./nextor/Mapper-ASCII8.noheader.bin \
+  /e:custom_code.bin \
+  /8:6000
+
+exit 0
